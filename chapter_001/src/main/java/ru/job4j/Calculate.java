0ca28ca641b62aa480169799cf899348ad228de8
ochudnovskiy <maxxwell131@yahoo.com>
2017-02-11 18:37:29 +0200

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,19 @@
+/**
+ * Calculator
+*/
 package ru.job4j;
 
+	/**
+	 *class Calculate решение задачи части 001 урок1.
+	 *@autor maxxwell131
+	 *@since 11.02.2017
+	*/
 public class Calculate {
 
+	/**
+	 *Главный метод Main
+	 *@param Strin[] args строковый массив входных данных
+	*/
 	public static void main(String[] args) {
 			System.out.println("Hello, java World");
 	}
